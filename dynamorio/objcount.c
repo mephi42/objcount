daef9689c6951c6ee88e73f6c0f11d7c49f41531
Ilya <mephi42@gmail.com>
2012-03-20 17:20:30 +0400

    timestamps and unload handling in dynamorio plugin
    Enhanced dynamorio plugin to:
    * write timestamps with millisecond resolution at regular intervals;
    * record module unload events;
    * include PID in output file name.
    
    Added analysis script that outputs number of allocated objects
    at different points in time.

--- dynamorio/objcount.c
+++ dynamorio/objcount.c
@@ -6,20 +6,35 @@
 #include <../ext/drwrap/drwrap.h>
 #include <string.h>
 
+/** Output file. */
 static file_t outFile;
 static void* outMutex = NULL;
 
+/** Active wrapper contexts. */
 static hashtable_t wraps;
+
+/** Number of milliseconds between timestamps. */
+static const uint64 timestampIntervalMillis = 100;
+
+/** Time to record next timestamp. */
+static uint64 nextTimestampMillis = 0;
+
 #define FLAG_BASE_OBJECT_DESTRUCTOR      ( 1 << 0 )
 #define FLAG_COMPLETE_OBJECT_DESTRUCTOR  ( 1 << 1 )
 #define FLAG_DEALLOCATING_DESTRUCTOR     ( 1 << 2 )
 #define FLAG_BASE_OBJECT_CONSTRUCTOR     ( 1 << 3 )
 #define FLAG_COMPLETE_OBJECT_CONSTRUCTOR ( 1 << 4 )
 #define FLAG_ALLOCATING_CONSTRUCTOR      ( 1 << 5 )
+
+static void wrapperPre( void* ctx, void** data );
+
+/** Wrapper context. */
 struct wrap {
   app_pc address;
   unsigned int flags;
 };
+
+/** Allocates storage for wrapper context. */
 static struct wrap* alloc_wrap() {
   struct wrap* wrap = ( struct wrap* )dr_global_alloc( sizeof( struct wrap ) );
   if( wrap == NULL ) {
@@ -28,19 +43,44 @@ static struct wrap* alloc_wrap() {
   }
   return wrap;
 }
-static void free_wrap( void* wrap ) {
+
+/** Deletes wrapper context and removes corresponding wrapper. */
+static void free_wrap( void* rawWrap ) {
+  struct wrap* wrap = ( struct wrap* )rawWrap;
+  if( !drwrap_unwrap( wrap->address, &wrapperPre, NULL ) ) {
+    dr_printf( "drwrap_unwrap(%p) failed\n", (void*)wrap->address );
+    exit( 1 );
+  }
   dr_global_free( wrap, sizeof( struct wrap ) );
 }
 
-static void wrapper( void* ctx, void** data ) {
+/** Wrapper pre-callback. */
+static void wrapperPre( void* ctx, void** data ) {
   dr_mutex_lock( outMutex );
-  dr_fprintf( outFile, "x %p\n", (void*)drwrap_get_func( ctx ) );
+
+  uint64 currentTimeMillis = dr_get_milliseconds();
+  if( currentTimeMillis > nextTimestampMillis ) {
+    dr_fprintf( outFile, "T 0x%.16llx\n", currentTimeMillis );
+    nextTimestampMillis = currentTimeMillis + timestampIntervalMillis;
+  }
+
+  dr_fprintf( outFile, "X %p %p\n", (void*)drwrap_get_func( ctx ),
+                                    drwrap_get_arg( ctx, 0 ) );
   dr_mutex_unlock( outMutex );
 }
 
-static void wrapSymbol( app_pc pc,
+/** Installs wrapper for a given symbol. */
+static void wrapSymbol( const char* sym,
+                        module_data_t* module,
+                        size_t offset,
                         struct demangle_component* qual,
                         struct demangle_component* info ) {
+  app_pc pc = module->start + offset;
+  if( offset == 0 || pc == 0 ) {
+    dr_printf( "Skipping symbol %s\n", sym );
+    return;
+  }
+
   size_t size;
   char* className = cplus_demangle_print( DMGL_NO_OPTS, qual, 64, &size );
 
@@ -48,13 +88,14 @@ static void wrapSymbol( app_pc pc,
   struct wrap* wrap = ( struct wrap* )hashtable_lookup( &wraps, (void*)pc );
   if( wrap == NULL ) {
     // Not wrapped yet.
-    if( !drwrap_wrap( pc, &wrapper, NULL ) ) {
-      dr_printf( "drwrap_wrap(%s %s of kind %i = %p) failed\n",
+    if( !drwrap_wrap( pc, &wrapperPre, NULL ) ) {
+      dr_printf( "drwrap_wrap(%s %s of kind %i = +%p = %p) failed\n",
                  className,
                  info->type == DEMANGLE_COMPONENT_CTOR ? "ctor" : "dtor",
                  info->type == DEMANGLE_COMPONENT_CTOR ? info->u.s_ctor.kind :
                                                          info->u.s_dtor.kind,
-                 pc );
+                 (void*)offset,
+                 (void*)pc );
       exit( 1 );
     }
 
@@ -107,8 +148,6 @@ static void wrapSymbol( app_pc pc,
 }
 
 static bool onSymbol( const char* sym, size_t offset, void* data ) {
-  app_pc pc = ((module_data_t*)data)->start + offset;
-
   if( sym[0] != '_' || sym[1] != 'Z' ) {
     // Not a mangled name - ignore.
     return true;
@@ -129,7 +168,7 @@ static bool onSymbol( const char* sym, size_t offset, void* data ) {
 
   if( info->type == DEMANGLE_COMPONENT_CTOR ||
       info->type == DEMANGLE_COMPONENT_DTOR ) {
-    wrapSymbol( pc, qual, info );
+    wrapSymbol( sym, (module_data_t*)data, offset, qual, info );
   }
 
   free( toFree );
@@ -137,11 +176,30 @@ static bool onSymbol( const char* sym, size_t offset, void* data ) {
 }
 
 static void onLoad( void* ctx, const module_data_t* m, bool loaded ) {
-  dr_printf( "In onLoad(%s)\n", m->full_path );
+  dr_printf( "In onLoad(%s, %p-%p)\n", m->full_path, m->start, m->end );
+
+  dr_mutex_lock( outMutex );
+  dr_fprintf( outFile, "M %p %p %s\n", m->start, m->end, m->full_path );
+  dr_mutex_unlock( outMutex );
+
   drsym_error_t rc = drsym_enumerate_symbols( m->full_path, onSymbol, (void*)m, 0 );
   if( DRSYM_SUCCESS != rc ) {
     dr_printf( "  drsym_enumerate_symbols() failed: %i\n", rc );
   }
+}
+
+static void onUnload( void* ctx, const module_data_t* m ) {
+  dr_printf( "In onUnload(%s, %p-%p)\n", m->full_path, m->start, m->end );
+
+  dr_mutex_lock( outMutex );
+  dr_fprintf( outFile, "m %p %p %s\n", (void*)m->start,
+                                       (void*)m->end,
+                                       m->full_path );
+  dr_mutex_unlock( outMutex );
+
+  hashtable_lock( &wraps );
+  hashtable_remove_range( &wraps, (void*)m->start, (void*)m->end );
+  hashtable_unlock( &wraps );
 }
 
 static void onExit() {
@@ -176,7 +234,11 @@ DR_EXPORT void dr_init( client_id_t id ) {
   }
 
   // Set up output.
-  outFile = dr_open_file( "objcount.out", DR_FILE_WRITE_OVERWRITE );
+  char fileName[256];
+  unsigned int pid = (unsigned int)dr_get_process_id();
+  dr_snprintf( fileName, sizeof( fileName ), "objcount-%u.out", pid );
+  fileName[sizeof( fileName ) - 1] = 0;
+  outFile = dr_open_file( fileName, DR_FILE_WRITE_OVERWRITE );
   outMutex = dr_mutex_create();
 
   // Set up hashtable.
